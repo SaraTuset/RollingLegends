dc04061aaade7f76499e9a35bf11f75315349bd2
NeoDaru <srodriguezgil1703@gmail.com>
2023-11-26 21:48:49 +0100

    añadir enlace imagen en pagina detalle y Service

--- Practica2/src/boardService.js
+++ Practica2/src/boardService.js
@@ -1,7 +1,7 @@
 const elementos = new Map();
 let nextId = 0;
 
-addElemento({ nombre: "Drurak", descripcion: "Drurak solía ser el dueño de una pequeña taberna en uno de los muchos barrios olvidados del reino de Yuwivia. Cuando empezó la guerra, su taverna fue calcinada junto con sus seres queridos. Ahora lucha con sus viejas armas para vengar a aquellos que perdieron su vida por culpa de esta injusta guerra", puntosVida: "50", ataque: "25", defensa: "25" });
+addElemento({ nombre: "Drurak", descripcion: "Drurak solía ser el dueño de una pequeña taberna en uno de los muchos barrios olvidados del reino de Yuwivia. Cuando empezó la guerra, su taverna fue calcinada junto con sus seres queridos. Ahora lucha con sus viejas armas para vengar a aquellos que perdieron su vida por culpa de esta injusta guerra", puntosVida: "50", ataque: "25", defensa: "25", imagen: "assets/img/dwarf.webp" });
 
 export function addElemento(elemento) {
     let id = nextId++;
