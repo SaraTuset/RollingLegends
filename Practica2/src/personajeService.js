cf75be628a49404a31c752dc2b2221d6e98603b7
jcl4332 <jcl4332@gmail.com>
2023-12-02 16:53:01 +0100

    error en css arreglado

--- Practica2/src/personajeService.js
+++ Practica2/src/personajeService.js
@@ -4,8 +4,8 @@ let subelementos = new Map();
 let nextSubId = 0;
 
 addElemento({ nombre: "Drurak", descripcion: "Drurak solía ser el dueño de una pequeña taberna en uno de los muchos barrios olvidados del reino de Yuwivia. Cuando empezó la guerra, su taverna fue calcinada junto con sus seres queridos. Ahora lucha con sus viejas armas para vengar a aquellos que perdieron su vida por culpa de esta injusta guerra", puntosVida: "50", ataque: "25", defensa: "25", imagen: "Practica2\assets\img\dwarf.webp", genero: "masculino" });
-addSubElemento({ id: 1, nombreSub: "Hacha Antigua", descripcionSub: "Hacha usada por los enanos del reino de Yuwivia", ataqueSub: "10", defensaSub: "0"});
-addSubElemento({ id: 1, nombreSub: "Escudo Espumoso", descripcionSub: "Escudo usada por los enanos del reino de Yuwivia, aunque este a sido modificado por Drurak para portar la imagen de una cerveza", ataqueSub: "0", defensaSub: "10"});
+addSubElemento({ id: 0, nombreSub: "Hacha Antigua", descripcionSub: "Hacha usada por los enanos del reino de Yuwivia", ataqueSub: "10", defensaSub: "0"});
+addSubElemento({ id: 0, nombreSub: "Escudo Espumoso", descripcionSub: "Escudo usada por los enanos del reino de Yuwivia, aunque este a sido modificado por Drurak para portar la imagen de una cerveza", ataqueSub: "0", defensaSub: "10"});
 
 addElemento({ nombre: "Jagger", descripcion: "'Míster Jägger' es uno de los youtubers y streamers que más dinero genera en nuestro país", puntosVida: "5000", ataque: "2500", defensa: "2500", imagen: "Practica2\assets\img\jagger.jpg", genero: "masculino" });
 
